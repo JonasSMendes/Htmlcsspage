ba8cee691cacb64b57ba720b7adc3d86f892aff3
jonas mendes <jonas.jsm2903@gmail.com>
2023-03-07 11:47:00 -0300

    alert

---
+++ Bootstrap_/assets/js/script.js
@@ -0,0 +1,13 @@
+
+const nome = document.querySelector('#nome').value;
+const botao = document.querySelector('#botao')
+
+
+botao.addEventListener('click', )
+
+console.log(nome)
+function mensagem(e){
+    
+    document.getElementById('mensagemFinal').innerHTML = `ola ${nome},${e}`
+}
+mensagem('tem uma mensagem pra voce')
